b7b6325ee130ee89cb2d30011ad3d376f9afdf84
Ishan2103 <ishanextra21@gmail.com>
2025-07-20 15:24:13 +0530

    Add files via upload

---
+++ Odd_Even.cpp
@@ -0,0 +1,18 @@
+//Ishan Danech
+//24070123047
+//ENTC A2
+#include <iostream>
+using namespace std;
+
+int main() {
+    int j;
+    cout << "Enter the number: ";
+    cin >> j;
+    if (j % 2 == 0) {
+        cout << "The given number is even." << endl;
+    } else {
+        cout << "The given number is odd." << endl;
+    }
+
+    return 0;
+}
